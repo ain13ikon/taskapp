5c378b4136dcb6ae59ef4fe132c05cec122e4d93
ain <ain.git13@gmail.com>
2018-09-07 18:41:39 +0900

    完成：基本動作

---
+++ taskapp/Task.swift
@@ -0,0 +1,34 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by きたむら on 2018/09/07.
+//  Copyright © 2018年 ain13ikon. All rights reserved.
+//
+
+//import Foundation 不要
+
+import RealmSwift
+
+class Task: Object {
+    //管理用ID。　プライマリーキー
+    @objc dynamic var id = 0
+    
+    //タイトル
+    @objc dynamic var title = ""
+    
+    //内容
+    @objc dynamic var contents = ""
+    
+    //日時
+    @objc dynamic var date = Date()
+    
+    /**
+        idをプライマリーキーとして設定
+    */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+    
+}
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,10 +7,19 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    //Realmインスタンスを取得する
+    let realm = try! Realm()
+    
+    //DB内のタスクが格納されるリスト
+    //日付が近い順でソート：降順
+    //以降内容をアップデートするとリスト内は自動的に更新される。
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: false)
     
     
     override func viewDidLoad() {
@@ -26,25 +35,59 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Dispose of any resources that can be recreated.
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController: InputViewController = segue.destination as! InputViewController
+        
+        if segue.identifier == "cellSegue"{
+            print("デバッグ：　セルのタップから移動します")
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        }else{
+            print("デバッグ：　＋から移動します")
+            let task = Task()
+            task.date = Date()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            
+            inputViewController.task = task
+        }
+    }
+    
     //UITableViewDataSourceのプロトコルメソッドを記述
-    //UITableViewDataSourceのプロトコルメソッド
+    //データの数を返すメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 0
+        return taskArray.count
     }
     
     //セルの内容を返すメソッド
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        //再利用可能なcellを得る
         //CellはパーツのCellのプロパティで自分で設定した値
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        //Cellに値を設定する
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString: String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
+        
         return cell
     }
     
     //UITableViewDelegateのプロトコルメソッド
     //セルを選択した時に実行されるメソッド
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        performSegue(withIdentifier: "cellSegue", sender: nil)
         print("デバッグ：　セルがタップされました")
+        performSegue(withIdentifier: "cellSegue", sender: nil)
     }
+    
     //セルが削除可能なことを伝えるメソッド
     func tableView(_ tableView: UITableView, editingStyleForRowAt: IndexPath) -> UITableViewCellEditingStyle {
         return .delete
@@ -52,6 +95,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     //Deleteボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            //データベースから削除する
+            try! realm.write {
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
+    }
+    
+    //入力画面から戻ってきた時にTableViewを更新させる
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
 
     
