b57e7bd6858c454dd0307d99f72c50bf568c8c21
ain <ain.git13@gmail.com>
2018-09-07 12:14:52 +0900

    4.2画面作成まで。【！】セルから画面遷移しない。

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -8,17 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //UITableViewDataSourceのプロトコルメソッドを記述
+    //UITableViewDataSourceのプロトコルメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 0
+    }
+    
+    //セルの内容を返すメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        //CellはパーツのCellのプロパティで自分で設定した値
+        return cell
+    }
+    
+    //UITableViewDelegateのプロトコルメソッド
+    //セルを選択した時に実行されるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        performSegue(withIdentifier: "cellSegue", sender: nil)
+        print("デバッグ：　セルがタップされました")
+    }
+    //セルが削除可能なことを伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForRowAt: IndexPath) -> UITableViewCellEditingStyle {
+        return .delete
+    }
+    
+    //Deleteボタンが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    }
+
+    
+
 
 
 }
